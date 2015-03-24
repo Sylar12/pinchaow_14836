5e481c872d5c9254794eb8060d9e422c5f78272b
Sylar12 <pinchaowang0422@gmail.com>
2015-03-24 01:19:15 -0700

    add arrow, change code

---
+++ Legolas.spritebuilder/Source/Arrow.h
@@ -0,0 +1,13 @@
+//
+//  Arrow.h
+//  Legolas
+//
+//  Created by PinchaoWang on 15/3/21.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Arrow : CCSprite
+
+@end
---
+++ Legolas.spritebuilder/Source/Arrow.m
@@ -0,0 +1,23 @@
+//
+//  Arrow.m
+//  Legolas
+//
+//  Created by PinchaoWang on 15/3/21.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "Arrow.h"
+
+@implementation Arrow
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Arrow created");
+    }
+    
+    return self;
+}
+
+@end
